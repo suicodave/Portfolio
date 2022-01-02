1f647e0d72b2acb940417b4e190955ab5a2d5280
suicodave <suico.dave@gmail.com>
2022-01-02 18:58:56 +0800

    added chips

--- src/app/components/greeting/greeting.component.ts
+++ src/app/components/greeting/greeting.component.ts
@@ -3,32 +3,44 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-greeting',
   template: `
-
     <div class="">
-      <h3 class="text-6xl text-indigo-50 font-400">
-        Meet Dave!
-      </h3>
+      <h3 class="text-6xl text-indigo-50 font-400">Meet Dave!</h3>
 
-      <p class="text-4xl text-indigo-100">
-        -A Full-Stack Web Developer.
-      </p>
+      <p class="text-4xl text-indigo-100">-A Full-Stack Web Developer.</p>
 
       <p class="text-xl mt-24 lg:w-4/5 text-justify">
-        Build apps to digitize business data from data-entry to monitoring and reporting dashboards to help the management create an informed decisions.
+        Build apps to digitize business data from data-entry to monitoring and
+        reporting dashboards to help the management create an informed
+        decisions.
       </p>
 
-      <a href="#aboutMe" mat-raised-button class="bg-purple-500 text-white mt-5">Learn More</a>
+      <div class="my-8">
+        <mat-chip-list aria-label="Fish selection">
+          <mat-chip color="accent" selected *ngFor="let item of niches">{{
+            item
+          }}</mat-chip>
+        </mat-chip-list>
+      </div>
 
+      <a href="#aboutMe" mat-raised-button class="bg-purple-500 text-white mt-5"
+        >Learn More</a
+      >
     </div>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class GreetingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  niches: string[] = [
+    '#Data Engineering',
+    '#Software Development',
+    '#Spreadsheets',
+    '#Integration',
+    '#Digitizaton',
+    '#Process Automation',
+    '#Reporting',
+  ];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,18 +3,16 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatChipsModule } from '@angular/material/chips';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
     MatCardModule,
-    MatDividerModule
+    MatDividerModule,
+    MatChipsModule,
   ],
-  exports: [
-    MatButtonModule,
-    MatCardModule,
-    MatDividerModule
-  ]
+  exports: [MatButtonModule, MatCardModule, MatDividerModule, MatChipsModule],
 })
-export class MaterialModule { }
+export class MaterialModule {}
